8f707da9052e2e720718582e25aca8f60580728d
Igor Malyarov <igor1309@gmail.com>
2023-03-05 22:46:31 +0300

    edit package, move solution to module

--- Package.swift
+++ Package.swift
@@ -4,16 +4,38 @@ import PackageDescription
 
 let package = Package(
     name: "CustomSegmentedPicker",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        .library(
-            name: "CustomSegmentedPicker",
-            targets: ["CustomSegmentedPicker"]),
+        .segmentedPicker,
     ],
     dependencies: [
     ],
     targets: [
-        .target(
-            name: "CustomSegmentedPicker",
-            dependencies: []),
+        .customSegmentedPicker,
+        .example1,
     ]
 )
+
+private extension Product {
+    
+    static let segmentedPicker = library(
+        name: .segmentedPicker,
+        targets: [.customSegmentedPicker]
+    )
+}
+
+private extension Target {
+    
+    static let customSegmentedPicker = target(name: .customSegmentedPicker)
+    static let example1 = target(name: .example1)
+}
+
+private extension String {
+    
+    static let customSegmentedPicker = "CustomSegmentedPicker"
+    static let segmentedPicker = "SegmentedPicker"
+    
+    static let example1 = "Example1"
+}
